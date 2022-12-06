b7ea6ad04e8b3bd3c2dc802e16dff4bb2a97f237
denitsadonova <denitsadonova@gmail.com>
2022-12-06 11:21:33 +0000

    Create Character_Sequence.java

---
+++ For_Loop/Character_Sequence.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Character_Sequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        for (int i = 0; i < text.length() ; i++) {
+            char symbol = text.charAt(i);
+            System.out.println(symbol);
+        }
+    }
+}
