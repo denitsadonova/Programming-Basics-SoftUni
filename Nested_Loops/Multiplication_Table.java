3c5babdc11c1226252b18f7def521583fe7f28ca
denitsadonova <denitsadonova@gmail.com>
2022-12-13 11:20:10 +0000

    Create Multiplication_Table.java

---
+++ Nested_Loops/Multiplication_Table.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Multiplication_Table {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+
+                System.out.printf("%d * %d = %d%n", i, j, i * j);
+
+            }
+
+        }
+    }
+}
