9f4fea1576ee3b8092649a6c69f3902679de6561
denitsadonova <denitsadonova@gmail.com>
2022-12-13 11:19:47 +0000

    Create Clock.java

---
+++ Nested_Loops/Clock.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Clock {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int hour = 0; hour <= 23; hour++) {
+            for (int min = 0; min <= 59; min++) {
+                System.out.printf("%d:%d%n", hour, min);
+            }
+
+        }
+    }
+}
