2357e2aa1faf17d68fa92ae892be58536c061b1a
denitsadonova <denitsadonova@gmail.com>
2023-01-05 11:54:16 +0000

    Create Multiplication_Table_2.java

---
+++ Basic_Syntax/Multiplication_Table_2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Multiplication_Table_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        int times = Integer.parseInt(scanner.nextLine());
+
+        do {
+            System.out.printf("%d X %d = %d%n", num, times, num * times);
+            times++;
+        } while (times <= 10);
+    }
+}
