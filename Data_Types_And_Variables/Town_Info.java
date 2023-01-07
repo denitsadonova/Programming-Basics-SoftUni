934b168177acde983b6b0e080df9ff73b26367ff
denitsadonova <denitsadonova@gmail.com>
2023-01-07 12:06:42 +0000

    Create Town_Info.java

---
+++ Data_Types_And_Variables/Town_Info.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Town_Info {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String townName = scanner.nextLine();
+        int population = Integer.parseInt(scanner.nextLine());
+        short area = Short.parseShort(scanner.nextLine());
+
+        String result = String.format
+                ("Town %s has population of %d and area %d square km.",
+                        townName, population, area);
+
+        System.out.println(result);
+    }
+}
